b798525a46769451dc8b84fdff4519b310ca1df6
dexterlabora <cguynn@gmail.com>
2020-08-27 16:20:27 +0200

    gh deploy

--- nuxt/nuxt.static.config.js
+++ nuxt/nuxt.static.config.js
@@ -13,10 +13,13 @@ export default {
   */
   target: 'static',
   router: {
-    base: '/pwa/'
+    base: '/nuxt/'
   },
   env: {
     baseUrl: process.env.BASE_URL || "http://localhost:3000"
+  },
+  generate: {
+    dir: 'docs'
   },
   serverMiddleware: ["~/api"],
   /*
