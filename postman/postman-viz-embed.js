0e5290a69b24588e47eaae2d948c7adb652e180e
dexterlabora <cguynn@gmail.com>
2020-09-07 11:42:34 +0200

    updated postman viz embed url

--- postman/postman-viz-embed.js
+++ postman/postman-viz-embed.js
@@ -1,6 +1,4 @@
-
 // Load remote template
-pm.sendRequest('postman-viz-jsonmagic-template.html',  (_, res) => ( temp = res.text()));
+pm.sendRequest('https://raw.githubusercontent.com/dexterlabora/json-magic/master/postman/postman-viz-jsonmagic-template.html',  (_, res) => ( temp = res.text()));
 // Run template
 setTimeout(() => pm.visualizer.set(temp, pm.response.json()),250)
-
