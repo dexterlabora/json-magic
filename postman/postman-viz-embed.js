114bcde9c9c3dbf8b8b5ba05dcfaf6f7936d06d0
dexterlabora <cguynn@gmail.com>
2020-09-07 12:06:33 +0200

    adjust style

--- postman/postman-viz-embed.js
+++ postman/postman-viz-embed.js
@@ -1,4 +1,4 @@
 // Load remote template
 pm.sendRequest('https://raw.githubusercontent.com/dexterlabora/json-magic/master/postman/postman-viz-jsonmagic-template.html',  (_, res) => ( temp = res.text()));
 // Run template
-setTimeout(() => pm.visualizer.set(temp, pm.response.json()),250)
+setTimeout(() => pm.visualizer.set(temp, pm.response.json()),300)
