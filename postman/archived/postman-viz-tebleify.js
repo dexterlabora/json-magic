c17b3c4c2f7260f21b29d59260ce08e49441ef1d
Cory Guynn <cguynn@gmail.com>
2023-05-10 18:00:40 +0200

    refactored  to support Postman compatability

---
+++ postman/archived/postman-viz-tebleify.js
@@ -0,0 +1,65 @@
+// Access the response data JSON as a JavaScript object
+const res = pm.response.json();
+
+
+// ------------
+// - Template -
+// ------------
+
+// Configure the template
+var template = `
+<button onclick="download()">Download Table </button><span><a id="magiclink" href="#">Open with JSON Magic</a>
+<div id="myTable" height="75"></div>
+
+
+ <!-- Tableify-->
+ 
+<script src="https://cdn.jsdelivr.net/npm/tableify@1.1.0/index.min.js"></script>
+ 
+
+<script>
+    var result;
+    var table
+    
+    function download () {
+      const data = table;
+      const element = document.createElement('a')
+      element.setAttribute(
+        'href',
+        'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(data, null, 4))
+      )
+      element.setAttribute('download', 'table.html')
+      element.style.display = 'none'
+      document.body.appendChild(element)
+      element.click()
+      document.body.removeChild(element)
+    }
+
+    function setMagicLink(data){
+        var ctx = document.getElementById("magiclink");
+        var uriData = encodeURIComponent(JSON.stringify(data));
+        ctx.href = "https://dexterlabora.github.io/json-magic/?hideInput=false&postman=true";
+    }
+
+    // Get DOM element to render the chart in
+    var ctx = document.getElementById("myTable");
+
+
+    // Access the data passed to pm.visualizer.set() from the JavaScript
+    // code of the Visualizer template
+    pm.getData(function (err, value) {
+        result = value;
+        table = tableify(value)
+        setMagicLink(result)
+       ctx.innerHTML = table
+    });
+
+</script>
+`;
+
+// -------------------------
+// - Bind data to template -
+// -------------------------
+
+// Set the visualizer template
+pm.visualizer.set(template, res);
--- postman/postman-viz-embed.js
+++
@@ -1,9 +0,0 @@
-// JSON Magic Postman Template
-const templateUrl = "https://raw.githubusercontent.com/dexterlabora/json-magic/master/postman/postman-viz-jsonmagic-template.html"
-
-// Load template
-pm.sendRequest(templateUrl,  (_, res) => { 
-    var template = res.text()
-    var data = pm.response.json()
-    pm.visualizer.set(template, data )
-});
